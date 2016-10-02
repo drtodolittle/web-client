6b3ec91600d06666619b6691dd1c8fcfbec7346b
hscsf <helge.schneider@udo.edu>
2016-10-02 19:48:42 +0200

    Testing.

--- src/main/resources/js/drtodolittle.js
+++ src/main/resources/js/drtodolittle.js
@@ -32,7 +32,7 @@ tdapp.config(["$routeProvider", "$locationProvider", "$compileProvider", "$httpP
       })
 			.when('/chpwd', {
         templateUrl : 'chpwd.html',
-				controller : 'SettingsCtrl'
+				controller : 'chpwdCtrl'
       })
 			.when('/egg', {
         templateUrl : 'egg.html'
@@ -65,6 +65,66 @@ tdapp.value(
 		errormsg : ""
 	}
 );
+
+tdapp.controller("chpwdCtrl",["$rootScope", "$scope", "$http", "localStorageService", "$route", "$location", function($rootScope,$scope,$http,localStorageService,$route,$location){
+
+    // Modification of $location.path()
+
+    var original = $location.path
+    $location.path = function(path,reload){
+        if (reload === false){
+            var lastRoute = $route.current
+            var un = $rootScope.$on('$locationChangeSuccess', function(){
+                $route.current = lastRoute
+                un()
+            });
+        }
+        return original.apply($location,[path])
+    }
+
+    // Change it...
+
+    $scope.doChPwd = function(){
+        console.log('doChPwd...')
+        if(
+			$scope.oldPassword==undefined ||
+			$scope.newPassword==undefined
+		){
+            console.log('Error: Enter valid data.')
+            $route.reload()
+            return
+		}
+        var user = firebase.auth().currentUser;
+		firebase.auth().signInWithEmailAndPassword(
+			user.email,
+			$scope.oldPassword
+		).then(
+			function(fbuser){
+				fbuser.updatePassword(
+					$scope.newPassword
+				).then(
+					function(){
+						alert("Password change done!");
+                        $route.reload()
+					}
+				).catch(function(error){
+					var errmsg = "Error: " + error.message;
+                    console.log(errmsg)
+                    $route.reload()
+				})
+                $route.reload()
+			}
+		).catch(function(error){
+			var errmsg = "Error: " + error.message;
+            console.log(errmsg)
+            $route.reload()
+		});
+    }
+
+    $location.path('/',false)
+
+    $('#oldpassword').focus()
+}]);
 
 /*
 
@@ -197,10 +257,11 @@ tdapp.controller("MainCtrl",
 
 /*
 
-	tdapp_controller_settings.js
+	Dr ToDo Little
+	Navigation controller
 
 */
-tdapp.controller("navigation",["$scope", "$http", "localStorageService", "$route", function($scope,$http,localStorageService, $route){
+tdapp.controller("navigation",["$scope", "$http", "localStorageService", "$route", "$location", "$timeout", function($scope,$http,localStorageService,$route,$location,$timeout){
 
 	$scope.logout = function() {
 		localStorageService.remove("logintoken");
@@ -208,6 +269,18 @@ tdapp.controller("navigation",["$scope", "$http", "localStorageService", "$route
 		$scope.password = "";
 		$route.reload();
 	}
+
+	$scope.changepassword = function(){
+		console.log('changepassword...');
+		$location.path('/chpwd');
+	}
+
+	$scope.home = function(){
+		console.log('home...');
+		$location.path('/');
+		$route.reload();
+	}
+
 }]);
 
 /*

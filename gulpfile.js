9363412de48b0a80376878a948310aaef7951f22
CloudyDirk <Guenther_D@de.cenit-group.com>
2016-10-09 11:58:35 +0200

    JS Maps mitkopiert

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,9 @@ gulp.task('bowerfonts', function() {
 gulp.task('bowerjs', function() {
   gulp.src([
     config.bowerDir + '/angular/angular.min.js',
+    config.bowerDir + '/angular/angular.min.js.map',
     config.bowerDir + '/angular-route/angular-route.min.js',
+    config.bowerDir + '/angular-route/angular-route.min.js.map',
     config.bowerDir + '/jquery/dist/jquery.min.js',
     config.bowerDir + '/bootstrap/dist/js/bootstrap.min.js',
     config.bowerDir + '/firebase/firebase.js',

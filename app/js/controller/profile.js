de9df36094ba3b73208d1e79197e9139b726f715
hscsf <helge.schneider@udo.edu>
2016-10-17 11:26:16 +0200

    Analyse-Profile.
    Anpassung Profile-Anzeige an globale Änderungen.

--- app/js/controller/profile.js
+++ app/js/controller/profile.js
@@ -6,43 +6,13 @@
 */
 tdapp.controller("profileCtrl",function($rootScope,$scope,$http,localStorageService,$route,$location){
 
-    // Modification of $location.path()
-
-    var original = $location.path
-    $location.path = function(path,reload){
-        if (reload === false){
-            var lastRoute = $route.current
-            var un = $rootScope.$on('$locationChangeSuccess', function(){
-                $route.current = lastRoute
-                un()
-            });
-        }
-        return original.apply($location,[path])
-    }
-
-    function showError(msg){
-        var ee = $('#errtemplate').clone()
-        ee.children('#errmsg').html(msg)
-        ee.css('visibility','visible')
-        $('#nfo').append(ee)
-    }
-
-    function showSuccess(msg){
-        var ee = $('#successtemplate').clone()
-        ee.children('#sucmsg').html(msg)
-        ee.css('visibility','visible')
-        $('#nfo').append(ee)
-    }
-
     var user = firebase.auth().currentUser
     if(user){
-        console.log('Analysis: User = ' + user.email)
         $scope.user = user.email
     } else {
-        console.log('Analysis: User = undefined (error in profile.js)')
-        showError('Try again later.')
+        $scope.user = "n/a"
     }
 
-    $location.path('/',false)
+    // TODO: Present error message to user.
 
 });

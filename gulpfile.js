9fa4b75913dbb0329056b0af3b3eb9fdf9497b35
hscsf <helge.schneider@udo.edu>
2017-01-02 22:29:42 +0100

    Anpassung.
    Angular-Mocks wird nun für die QUnit-Tests verwendet.

--- gulpfile.js
+++ gulpfile.js
@@ -69,6 +69,7 @@ gulp.task('bowerjs', function() {
   gulp.src([
     config.bowerDir + '/angular/angular.min.js',
     config.bowerDir + '/angular/angular.js',
+    config.bowerDir + '/angular/angular-mocks.js',
     config.bowerDir + '/angular/angular.min.js.map',
     config.bowerDir + '/angular-route/angular-route.min.js',
     config.bowerDir + '/angular-route/angular-route.js',

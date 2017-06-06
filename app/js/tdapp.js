7cff90dbd0b83b690eecabcf00f56abc4498e6f8
Dirk Günther <d.guenther@cloud57.de>
2017-06-06 20:16:44 +0200

    URL für sloppy geändert

--- app/js/tdapp.js
+++ app/js/tdapp.js
@@ -66,7 +66,7 @@ tdapp.config(function(
 
 // Check for test envirionment
 
-var serverurl = "http://localhost:8181/api/todos";
+var serverurl = "https://rest-api.sloppy.zone/api/todos";
 if (window.location.host.startsWith("test")) {
     serverurl = "http://test.drtodolittle.de/api/todos";
 }

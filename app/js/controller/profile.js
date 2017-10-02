5d55036f0271337a677cbd10507997e947545a33
hscsf <h.schneider@cloud57.de>
2017-10-02 11:13:10 +0200

    Display number of todos in profile.

--- app/js/controller/profile.js
+++ app/js/controller/profile.js
@@ -10,21 +10,29 @@ tdapp.controller("profileCtrl",function(
     $http,
     localStorageService,
     $route,
-    $location
+    $location,
+    todoservice
 ){
 
     var user = firebase.auth().currentUser
     if(user){
+        todoservice.getTodos().then(function(todos) {
+            $scope.numOfTodos = todos.length;
+        }).catch(function(err){
+            $scope.numOfTodos = "n/a";
+            // TODO: Presetn error message to user.
+        });
         if(user.providerData.length>0){
-            $scope.user = user.providerData[0].email
-            $scope.userphotourl = user.providerData[0].photoURL
+            $scope.user = user.providerData[0].email;
+            $scope.userphotourl = user.providerData[0].photoURL;
         } else {
-            $scope.user = "n/a"
-            $scope.userphotourl = "null"
+            $scope.user = "n/a";
+            $scope.userphotourl = "null";
         }
     } else {
-        $scope.user = "n/a"
-        $scope.userphotourl = "null"
+        $scope.user = "n/a";
+        $scope.userphotourl = "null";
+        $scope.numOfTodos = "n/a";
     }
 
     // TODO: Present error message (if occured) to user.

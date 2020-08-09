601d139fb3042bab19afbb62e0ba566da253de1e
Dirk Günther <d.guenther@cloud57.de>
2020-08-09 20:50:27 +0200

    Workbox integrated

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -1,1 +1,10 @@
-console.log('Serviceworker');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+
+  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.{ico,png,html,js,webmanifest,css}"
+  ],
+  "swDest": "dist/js/service-worker.js",
+  "swSrc": "src/js/service-worker.js"
+};

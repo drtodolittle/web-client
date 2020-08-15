d2aac249dc7b4eb4f26210226304f4faac2261f1
Dirk Günther <d.guenther@cloud57.de>
2020-08-15 09:36:54 +0200

    Design angepasst

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js');
 
-if (workbox) {
+if (workbox && !self.location.hostname.startsWith('localhost')) {
   workbox.core.skipWaiting()
   workbox.core.clientsClaim()
   workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
